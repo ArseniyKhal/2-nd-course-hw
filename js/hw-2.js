29e0c6be3abf3c75d930abab2517a148f779eee7
ArseniyH <asmn@mail.ru>
2023-03-24 15:39:49 +0500

    hw2

---
+++ js/hw-2.js
@@ -0,0 +1,60 @@
+//Задание 1
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+//Задание 2
+// let birthYearIphone = 2007;
+// alert("года выпуска первого iPhone " + birthYearIphone + " год");
+
+//Задание 3
+// const creatorJS = "Brendan Eich";
+// alert(`Создателем языка JavaScript является ${creatorJS}`);
+
+//Задание 4
+// let numberOne = 10;
+// let numberTwo = 2;
+// alert(`Сумма двух чисел равна ${numberOne + numberTwo}\nРазность этих чисел равна ${numberOne - numberTwo}\nПроизведение этих чисел равна ${numberOne * numberTwo}\nЧастное этих чисел равна ${numberOne / numberTwo}`);
+
+//Задание 5
+// let kakaiaToPeremennaia = 2;
+// let result = kakaiaToPeremennaia ** 5;
+// alert(result);
+
+//Задание 6
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+//Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+// Задание 8
+// let age = prompt("Сколько вам лет?");
+// alert(`Вам ${age} лет`);
+
+/*Задание 9
+let user = {
+	name: 'Вова',
+	age: 50,
+	isAdmin: true,
+}
+user['city of residence'] = 'Moscow';
+user.age = 44;
+delete user['city of residence'];
+let info = prompt("Какую информацию хотите узнать о пользователе?", "name or age or isAdmin");
+alert(user[info]);
+*/
+
+//Задание 10
+let name = prompt("Твоё имя ?");
+alert(`Привет, ${name}!`);
+
